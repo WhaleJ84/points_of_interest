884ecb4f5637034bc30f29a0aa395dd5e894890c
James Whale <james@james-whale.com>
2020-03-30 13:46:04 +0100

    made the initial switch to json-parsing

--- index.php
+++ index.php
@@ -20,38 +20,37 @@ $app->addErrorMiddleware(true, true, true);
 // Set base path to website root directory
 $app->setBasePath('/pointsofinterest');
 
-$conn=new PDO('mysql:host=localhost;dbname=assign225','assign225','umoodahc');
+$conn=new PDO('mysql:host=localhost;dbname=assign225', 'assign225', 'umoodahc');
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 // Create our PHP renderer object
 $view=new \Slim\Views\PhpRenderer('views');
 
 // PointsOfInterest pages
-$app->get('/', function(Request $req, Response $res, array $args) use($conn,$view){
+$app->get('/', function (Request $req, Response $res, array $args) use ($conn,$view) {
     unset($_SESSION['pageID']);
     $regions=$conn->prepare('SELECT DISTINCT region FROM pointsofinterest');
     $regions->execute();
-    $statement=$conn->prepare('SELECT * FROM pointsofinterest ORDER BY recommended DESC');
-    $statement->execute();
-    $res=$view->render($res, 'points_of_interest.phtml', ['results'=>$statement, 'regions'=>$regions]);
+    $res=$view->render($res, 'points_of_interest.phtml', ['regions'=>$regions]);
     return $res;
 });
 
-$app->get('/get_poi', function(Request $req, Response $res, array $args) use($conn,$view){
+$app->get('/get_poi', function (Request $req, Response $res, array $args) use ($conn,$view) {
     unset($_SESSION['pageID']);
     $statement=$conn->prepare('SELECT * FROM pointsofinterest ORDER BY recommended DESC');
     $statement->execute();
-    $res=$view->render($res, 'get_poi.phtml', ['results'=>$statement]);
-    return $res;
+    $results=$statement->fetchAll(PDO::FETCH_ASSOC);
+    //$res=$view->render($res, 'get_poi.phtml', ['results'=>$statement]);
+    return $res->withJson($results);
 });
 
-$app->get('/get_review', function(Request $req, Response $res, array $args) use($conn,$view){
+$app->get('/get_review', function (Request $req, Response $res, array $args) use ($conn,$view) {
     $reviews=$conn->prepare('SELECT * FROM poi_reviews WHERE approved=1');
     $reviews->execute();
     $res=$view->render($res, 'get_review.phtml', ['reviews'=>$reviews]);
     return $res;
 });
 
-$app->get('/region/{region}', function(Request $req, Response $res, array $args) use($conn,$view){
+$app->get('/region/{region}', function (Request $req, Response $res, array $args) use ($conn,$view) {
     unset($_SESSION['pageID']);
     $statement=$conn->prepare('SELECT * FROM pointsofinterest WHERE region=? ORDER BY recommended DESC');
     $statement->execute([$args['region']]);
@@ -59,7 +58,7 @@ $app->get('/region/{region}', function(Request $req, Response $res, array $args)
     return $res;
 });
 
-$app->get('/view/{id}', function(Request $req, Response $res, array $args) use($conn,$view){
+$app->get('/view/{id}', function (Request $req, Response $res, array $args) use ($conn,$view) {
     $_SESSION['pageID']=$args['id'];
     $reviews=$conn->prepare('SELECT * FROM poi_reviews WHERE approved=1 ORDER BY approved ASC');
     $reviews->execute();
@@ -69,9 +68,9 @@ $app->get('/view/{id}', function(Request $req, Response $res, array $args) use($
     return $res;
 });
 
-$app->get('/admin', function(Request $req, Response $res, array $args) use($conn,$view){
-    if (!isset($_SESSION['isadmin'])){
-        return $res->withHeader('Location','/pointsofinterest');
+$app->get('/admin', function (Request $req, Response $res, array $args) use ($conn,$view) {
+    if (!isset($_SESSION['isadmin'])) {
+        return $res->withHeader('Location', '/pointsofinterest');
     }
     $reviews=$conn->prepare('SELECT * FROM poi_reviews WHERE approved=0 ORDER BY approved ASC');
     $reviews->execute();
@@ -79,14 +78,14 @@ $app->get('/admin', function(Request $req, Response $res, array $args) use($conn
     return $res;
 });
 
-$app->post('/admin/approve', function(Request $req, Response $res, array $args) use($conn){
+$app->post('/admin/approve', function (Request $req, Response $res, array $args) use ($conn) {
     $post=$req->getParsedBody();
     $approve=$conn->prepare('UPDATE poi_reviews SET approved=1 WHERE id=?');
     $approve->execute([$post['id']]);
     return $res->withHeader('Location', '/pointsofinterest/admin');
 });
 
-$app->post('/recommend', function(Request $req, Response $res, array $args) use($conn){
+$app->post('/recommend', function (Request $req, Response $res, array $args) use ($conn) {
     $post=$req->getParsedBody();
     // Cannot redirect using $post['ID'] for some reason
     $statement=$conn->prepare('UPDATE pointsofinterest SET recommended=recommended+1 WHERE ID=?');
@@ -95,26 +94,26 @@ $app->post('/recommend', function(Request $req, Response $res, array $args) use(
     return $res;
 });
 
-$app->get('/add', function(Request $req, Response $res, array $args) use($view){
+$app->get('/add', function (Request $req, Response $res, array $args) use ($view) {
     $res=$view->render($res, 'add_poi.phtml');
     return $res;
 });
 
-$app->post('/add_poi', function(Request $req, Response $res, array $args) use($conn){
+$app->post('/add_poi', function (Request $req, Response $res, array $args) use ($conn) {
     $post=$req->getParsedBody();
     $statement=$conn->prepare('INSERT INTO pointsofinterest (name,type,country,region,lon,lat,description,username) VALUES (?,?,?,?,?,?,?,?)');
     $statement->execute([$post['name'],$post['type'],$post['country'],$post['region'],$post['lon'],$post['lat'],$post['description'],$post['username']]);
     return $res->withHeader('Location', '/pointsofinterest');
 });
 
-$app->get('/view/{id}/review', function(Request $req, Response $res, array $args) use($conn,$view){
+$app->get('/view/{id}/review', function (Request $req, Response $res, array $args) use ($conn,$view) {
     $statement=$conn->prepare('SELECT * FROM pointsofinterest WHERE ID=?');
     $statement->execute([$args['id']]);
     $res=$view->render($res, 'review_poi.phtml', ['results'=>$statement]);
     return $res;
 });
 
-$app->post('/review_poi', function(Request $req, Response $res, array $args) use($conn){
+$app->post('/review_poi', function (Request $req, Response $res, array $args) use ($conn) {
     $post=$req->getParsedBody();
     $ID=$post['poi_id'];
     $statement=$conn->prepare('INSERT INTO poi_reviews (poi_id,review) VALUES (?,?)');
@@ -123,36 +122,36 @@ $app->post('/review_poi', function(Request $req, Response $res, array $args) use
 });
 
 // User account pages
-$app->get('/accounts', function(Request $req, Response $res, array $args) use($view){
+$app->get('/accounts', function (Request $req, Response $res, array $args) use ($view) {
     $res=$view->render($res, '/accounts.phtml');
     return $res;
 });
 
-$app->post('/login', function(Request $req, Response $res, array $args) use($conn){
+$app->post('/login', function (Request $req, Response $res, array $args) use ($conn) {
     $post=$req->getParsedBody();
     $statement=$conn->prepare('SELECT * FROM poi_users WHERE username=? AND password=?');
     $statement->execute([$post['username'], $post['password']]);
     $row=$statement->fetch(PDO::FETCH_ASSOC);
     $_SESSION['gatekeeper']=$row['username'];
-    if ($row['isadmin'] == 1){
+    if ($row['isadmin'] == 1) {
         $_SESSION['isadmin']=1;
     }
     return $res->withHeader('Location', '/pointsofinterest');
 });
 
-$app->get('/logout', function(Request $req, Response $res, array $args){
+$app->get('/logout', function (Request $req, Response $res, array $args) {
     session_destroy();
     return $res->withHeader('Location', '/pointsofinterest');
 });
 
-$app->post('/password', function(Request $req, Response $res, array $args) use($conn){
+$app->post('/password', function (Request $req, Response $res, array $args) use ($conn) {
     $post=$req->getParsedBody();
     $statement=$conn->prepare('UPDATE poi_users SET password=? WHERE username=?');
     $statement->execute([$post['password'], $_SESSION['gatekeeper']]);
     return $res->withHeader('Location', '/pointsofinterest');
 });
 
-$app->post('/signup', function(Request $req, Response $res, array $args) use($conn){
+$app->post('/signup', function (Request $req, Response $res, array $args) use ($conn) {
     $post=$req->getParsedBody();
     $statement=$conn->prepare('INSERT INTO poi_users (username,password) VALUES (?,?)');
     $statement->execute([$post['username'], $post['password']]);
@@ -161,4 +160,3 @@ $app->post('/signup', function(Request $req, Response $res, array $args) use($co
 
 // Run the Slim app.
 $app->run();
-?>
--- script/ajax.js
+++ script/ajax.js
@@ -1,78 +1,86 @@
-window.onload = function(){
-    getPoi();
-    document.getElementById('regionButton').addEventListener('click', regionRequest);
-}
-
 function getPoi(){
-    var xhr2 = new XMLHttpRequest();
-    xhr2.addEventListener ('load', responseReceived);
-    xhr2.open('GET', '/pointsofinterest/get_poi');
-    xhr2.send();
-}
-
-function getReview(){
-    var review = new XMLHttpRequest();
-    review.addEventListener ('load', displayReviews);
-    review.open('GET', '/pointsofinterest/get_review');
-    review.send();
-}
-
-function recommend(id){
-    var recommend = new XMLHttpRequest();
-    recommend.addEventListener ('load', responseReceived);
-    recommend.open('POST', '/pointsofinterest/recommend');
-    recommend.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    recommend.send(ID=id);
+    var results = new XMLHttpRequest();
+    results.addEventListener('load', responseReceived);
+    results.open('GET', '/pointsofinterest/get_poi');
+    results.send();
 }
 
 function regionRequest(){
     // Create the XMLHttpRequest variable.
     // This variable represents the AJAX communication between client and server.
-    var xhr2 = new XMLHttpRequest();
+    var regions = new XMLHttpRequest();
 
     // Read the data from the form fields.
     var region = document.getElementById('region').value;
 
     // Specify the CALLBACK function.
     // When we get a response from the server, the callback function will run
-    xhr2.addEventListener ('load', responseReceived);
+    regions.addEventListener('load', responseReceived);
 
     // Open the connection to the server
-    // We are sending a request to "flights.php" in the same folder and passing in the destination and date as a query string.
-    xhr2.open('GET','/pointsofinterest/region/' + region);
+    // We are sending a request to "flights.php" in the same folder and passing in the destination and
+    // date as a query string.
+    regions.open('GET', `/pointsofinterest/region/${region}`);
 
     // Send the request.
-    xhr2.send();
+    regions.send();
 }
 
-function poiRequest(id){
-    // Create the XMLHttpRequest variable.
-    // This variable represents the AJAX communication between client and server.
-    var poi = new XMLHttpRequest();
-
-    // Read the data from the form fields.
-    //var id = document.getElementById('link').value;
-
-    // Specify the CALLBACK function.
-    // When we get a response from the server, the callback function will run
-    poi.addEventListener ('load', responseReceived);
-
-    // Open the connection to the server
-    // We are sending a request to "flights.php" in the same folder and passing in the destination and date as a query string.
-    poi.open('GET','/pointsofinterest/view/' + id);
-
-    // Send the request.
-    poi.send();
+function getReview(){
+    var review = new XMLHttpRequest();
+    review.addEventListener('load', displayReviews);
+    review.open('GET', '/pointsofinterest/get_review');
+    review.send();
 }
 
 // The callback function simply places the response from the server in the div with the ID of 'response'.
 // The parameter "e" contains the original XMLHttpRequest variable as "e.target".
 // We get the actual response from the server as "e.target.responseText"
 function responseReceived(e){
-    document.getElementById('searchresults').innerHTML = e.target.responseText;
+    // Parse the JSON into an array of JavaScript objects
+    var poiData = JSON.parse(e.target.responseText);
+
+    // Create a string array to store the results in
+    var results = '<table><tr><th>ID</th>' +
+    '<th>Name</th>' +
+    '<th>Type</th>' +
+    '<th>Country</th>' +
+    '<th>Region</th>' +
+    '<th>Longitude</th>' +
+    '<th>Latitude</th>' +
+    '<th>Description</th>' +
+    '<th>Recommended</th>' +
+    '<th>Username</th></tr>';
+
+    // Use a 'for' loop to loop through each result
+    // poiData will be an array of all the records inside the JSON, so
+    // poiData[i] will be the current result.
+    // We can then extract the individual fields with poiData[i].depart,
+    // poiData[i].arrive, etc.
+    for (var i = 0; i < poiData.length; i++) {
+        results = results + '<tr><td>' + poiData[i].ID + '</td>' +
+        '<td>' + poiData[i].name + '</td>' +
+        '<td>' + poiData[i].type + '</td>' +
+        '<td>' + poiData[i].country + '</td>' +
+        '<td>' + poiData[i].region + '</td>' +
+        '<td>' + poiData[i].lon + '</td>' +
+        '<td>' + poiData[i].lat + '</td>' +
+        '<td>' + poiData[i].description + '</td>' +
+        '<td>' + poiData[i].recommend + '</td>' +
+        '<td>' + poiData[i].username + '</td></tr>';
+    }
+
+    results = results + '</table>';
+
+    document.getElementById('searchresults').innerHTML = results;
     getReview();
 }
 
-function displayReviews(f){
+function displayReviews(f) {
     document.getElementById('reviews').innerHTML = f.target.responseText;
 }
+
+window.onload = function () {
+    getPoi();
+    document.getElementById('regionButton').addEventListener('click', regionRequest);
+};
